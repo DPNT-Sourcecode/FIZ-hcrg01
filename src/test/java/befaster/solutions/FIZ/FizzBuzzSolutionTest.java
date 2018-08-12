e2930fd4bbed14d9233f0385c61df14115b91263
sbx_user1060 <sbx_user1060@ip-10-12-192-83.eu-west-2.compute.internal>
2018-08-12 23:41:14 +0000

    Sun Aug 12 23:41:14 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -18,8 +18,7 @@ public class FizzBuzzSolutionTest {
 	@Test
 	public void fizzBuzzTest() {
 		assertEquals("fizz fake deluxe", fizzBuzz.fizzBuzz(3));
-		assertEquals("buzz deluxe", fizzBuzz.fizzBuzz(5));
-		assertEquals("buzz", fizzBuzz.fizzBuzz(52));
+		assertEquals("buzz fake deluxe", fizzBuzz.fizzBuzz(5));
 		assertEquals("fizz buzz fake deluxe", fizzBuzz.fizzBuzz(15));
 		assertEquals("fizz buzz deluxe", fizzBuzz.fizzBuzz(546));
 		assertEquals("fizz fake deluxe", fizzBuzz.fizzBuzz(33));
