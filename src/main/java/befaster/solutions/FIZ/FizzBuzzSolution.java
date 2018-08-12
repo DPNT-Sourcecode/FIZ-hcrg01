4faadbc55315737162b6f9c17e1a085d40f6ba72
sbx_user1060 <sbx_user1060@ip-10-12-192-83.eu-west-2.compute.internal>
2018-08-12 22:06:45 +0000

    Sun Aug 12 22:06:45 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -30,6 +30,23 @@ public class FizzBuzzSolution {
     	return number % (5 * 3) == 0 || (isFizz(number) && isBuzz(number));
     }
     
+    private boolean isDeluxe(Integer number) {
+    	return number > 10 && containsSameDigit(number);
+    }
+    
+    private boolean containsSameDigit(Integer number) {
+    	boolean deluxe = true;
+    	String str = String.valueOf(number);
+    	char first = str.charAt(0);
+    	for(int i = 1; i < str.length(); i++) {
+    		if(str.charAt(i) != first) {
+    			deluxe = false;
+    			break;
+    		}
+    	}
+    	return deluxe;
+    }
+    
     private boolean contains(Integer number, String numberToFind) {
     	String str = String.valueOf(number);
     	return str.indexOf(numberToFind) > -1 ? true : false;
