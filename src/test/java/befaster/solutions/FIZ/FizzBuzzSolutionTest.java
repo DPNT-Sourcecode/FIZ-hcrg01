0bde50cf43aa9a327c8e3d1e87dddaeacbdd6f12
sbx_user1060 <sbx_user1060@ip-10-12-192-83.eu-west-2.compute.internal>
2018-08-12 23:14:14 +0000

    Sun Aug 12 23:14:14 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -18,14 +18,13 @@ public class FizzBuzzSolutionTest {
 	@Test
 	public void fizzBuzzTest() {
 		assertEquals("fizz deluxe", fizzBuzz.fizzBuzz(3));
-		assertEquals("fizz", fizzBuzz.fizzBuzz(13));
+		assertEquals("fizz fake deluxe", fizzBuzz.fizzBuzz(13));
 		assertEquals("buzz deluxe", fizzBuzz.fizzBuzz(5));
 		assertEquals("buzz", fizzBuzz.fizzBuzz(52));
-		assertEquals("deluxe", fizzBuzz.fizzBuzz(22));
-		assertEquals("buzz deluxe", fizzBuzz.fizzBuzz(15));
-		assertEquals("fizz buzz", fizzBuzz.fizzBuzz(546));
+		assertEquals("fizz buzz fake deluxe", fizzBuzz.fizzBuzz(15));
+		assertEquals("fizz buzz deluxe", fizzBuzz.fizzBuzz(546));
 		assertEquals("fizz fake deluxe", fizzBuzz.fizzBuzz(33));
-		assertEquals("deluxe", fizzBuzz.fizzBuzz(222));
+		assertEquals("fizz", fizzBuzz.fizzBuzz(222));
 		assertEquals("fizz fake deluxe", fizzBuzz.fizzBuzz(777));
 		assertEquals("2", fizzBuzz.fizzBuzz(2));
 	}
