47b5b09e4e6de733e41c4aafd5e13b675078b15f
sbx_user1060 <sbx_user1060@ip-10-12-192-83.eu-west-2.compute.internal>
2018-08-12 22:39:25 +0000

    Sun Aug 12 22:39:25 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -8,16 +8,27 @@ public class FizzBuzzSolution {
 	private static final String FAKE_DELUXE = "fake deluxe";
 	private static final String FIZZ_BUZZ = "fizz buzz";
 	private static final String FIZZ_DELUXE = "fizz deluxe";
+	private static final String FIZZ_FAKE_DELUXE = "fizz fake deluxe";
 	private static final String BUZZ_DELUXE = "buzz deluxe";
+	private static final String BUZZ_FAKE_DELUXE = "buzz fake deluxe";
 	private static final String FIZZ_BUZZ_DELUXE = "fizz buzz deluxe";
+	private static final String FIZZ_BUZZ_FAKE_DELUXE = "fizz buzz fake deluxe";
 
     public String fizzBuzz(Integer number) {
-    	if(isFizzBuzzDeluxe(number)) {
+    	if(isFizzBuzzFakeDeluxe(number)) {
+    		return FIZZ_BUZZ_FAKE_DELUXE;
+    	} else if(isFizzBuzzDeluxe(number)) {
     		return FIZZ_BUZZ_DELUXE;
+    	} else if(isFizzFakeDeluxe(number)) {
+    		return FIZZ_FAKE_DELUXE;
+    	} else if(isBuzzFakeDeluxe(number)) {
+    		return BUZZ_FAKE_DELUXE;
     	} else if(isFizzDeluxe(number)) {
     		return FIZZ_DELUXE;
     	} else if(isBuzzDeluxe(number)) {
     		return BUZZ_DELUXE;
+    	} else if(isFakeDeluxe(number)) {
+    		return FAKE_DELUXE;
     	} else if(isDeluxe(number)) {
     		return DELUXE;
     	} else if (isFizzBuzz(number)) {
@@ -44,11 +55,15 @@ public class FizzBuzzSolution {
     }
     
     private boolean isFizzDeluxe(Integer number) {
-    	return (number % 3 == 0 || contains(number, "3")) && isDeluxe(number);
+    	return isFizz(number) && isDeluxe(number);
+    }
+    
+    private boolean isFizzFakeDeluxe(Integer number) {
+    	return isFizz(number) && isFakeDeluxe(number);
     }
     
     private boolean isBuzzDeluxe(Integer number) {
-    	return (number % 5 == 0 || contains(number, "5")) && isDeluxe(number);
+    	return isBuzz(number) && isDeluxe(number);
     }
     
     private boolean isDeluxe(Integer number) {
@@ -63,6 +78,10 @@ public class FizzBuzzSolution {
     	return isFizzBuzz(number) && isDeluxe(number);
     }
     
+    private boolean isFizzBuzzFakeDeluxe(Integer number) {
+    	return isFizzBuzz(number) && isFakeDeluxe(number);
+    }
+   
     private boolean containsSameDigit(Integer number) {
     	boolean deluxe = true;
     	String str = String.valueOf(number);
